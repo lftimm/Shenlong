308d6947ac40886aa1950b28a4d3ef61bf22ae4e
lftimm <lftimm1902@gmail.com>
2024-11-02 18:47:15 -0300

    web refatorado

--- main.py
+++
@@ -1,5 +0,0 @@
-import Shenlong
-from Shenlong import MyAnimeListHtmlSearch, MyAnimeListWebsite, SearchFilter
-
-s = Shenlong.search(MyAnimeListHtmlSearch(MyAnimeListWebsite), 'Naruto', SearchFilter(_score=8.0))
-print(s)
